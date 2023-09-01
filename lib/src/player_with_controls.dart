f7bae28d19d3396bc64147df153dd6ba00fdb1bc
Yash Sharma <yash.sharma@velotio.com>
2023-09-01 19:18:08 +0530

    feat: changes in height

--- lib/src/player_with_controls.dart
+++ lib/src/player_with_controls.dart
@@ -65,7 +65,7 @@ class PlayerWithControls extends StatelessWidget {
               child: chewieController.topControls!),
           if (chewieController.bottomControls != null)
             Positioned(
-              bottom: 50,
+              bottom: 40,
               child: SizedBox(
                  height: 250,
                   width: MediaQuery.of(context).size.width,
