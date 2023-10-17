9adb27fdaac1a540fbfc02221c7075274ab02ec7
Roots212 <yasjgh@gmail.com>
2023-10-17 15:02:29 +0530

    feat: changes in aspect ratio

--- lib/src/player_with_controls.dart
+++ lib/src/player_with_controls.dart
@@ -69,7 +69,7 @@ class PlayerWithControls extends StatelessWidget {
                           panEnabled: chewieController.zoomAndPan,
                           scaleEnabled: chewieController.zoomAndPan,
                           child: FittedBox(
-                              fit: BoxFit.cover,
+                              fit:canCompare?BoxFit.contain: BoxFit.cover,
                               clipBehavior: Clip.antiAlias,
                               child: ValueListenableBuilder<bool>(
                                 valueListenable: isInitialised,
